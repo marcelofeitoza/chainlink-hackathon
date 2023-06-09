3bdedab939b355701ebeb6aa1b886a8e3b19f5c8
vict0rcarvalh0 <victordecarvalho342@gmail.com>
2023-06-09 18:03:21 -0300

    feat: architecture image upload

--- contracts/DAO/TimeLock.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-import '../node_modules/@openzeppelin/contracts/governance/TimelockController.sol';
-
-contract TimeLock is TimelockController {
-    constructor(uint256 _minDelay, address[] memory _proposers, address[] memory _executors, address _token) TimelockController(_minDelay, _proposers, _executors, _token) {
-    }
-}
--- contracts/migrations/1_deployContract.js
+++ contracts/migrations/1_deployContract.js
@@ -6,9 +6,9 @@ const contractProfileImageFactory = artifacts.require("ProfileImageFactory");
 module.exports = function(deployer) {
     // deployment steps
     // const updateInterval = 60 * 60 * 24; // 1 day
-    const updateInterval = 120;
+    // const updateInterval = 120;
     deployer.deploy(contractDonation);
-    deployer.deploy(contractProfileImage, updateInterval);
+    // deployer.deploy(contractProfileImage, updateInterval);
     deployer.deploy(contractPost);
     // deployer.deploy(contractProfileImageFactory, updateInterval);
   };
