6b993037eadb15e60fe7f104e15ef7ca447de43b
vict0rcarvalh0 <victordecarvalho342@gmail.com>
2023-06-09 03:17:25 -0300

    fix: fixing factory function

--- contracts/contracts/User/ProfileImageFactory.sol
+++ contracts/contracts/User/ProfileImageFactory.sol
@@ -6,9 +6,10 @@ import "./ProfileImage.sol";
 contract ProfileImageFactory {
     address[] public userPhotoContracts;
 
-    function createUserPhoto(uint updateInterval) public {
+    function createUserPhoto(uint updateInterval) public returns (address) {
         ProfileDNFT newUserPhoto = new ProfileDNFT(updateInterval);
         userPhotoContracts.push(address(newUserPhoto));
+        return userPhotoContracts[userPhotoContracts.length - 1];
     }
 
     function getUserPhotoContracts() public view returns (address[] memory) {
