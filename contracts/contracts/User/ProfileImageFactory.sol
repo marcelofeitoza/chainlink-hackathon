c734484e60b4653d62f84280c46a6e5f4a5dcdfb
vict0rcarvalh0 <victordecarvalho342@gmail.com>
2023-06-09 03:53:55 -0300

    fix: fixing contract

--- contracts/contracts/User/ProfileImageFactory.sol
+++ contracts/contracts/User/ProfileImageFactory.sol
@@ -2,8 +2,10 @@
 pragma solidity ^0.8.9;
 
 import "./ProfileImage.sol";
+import "../../node_modules/@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 
-contract ProfileImageFactory {
+
+contract ProfileImageFactory is IERC721Receiver{
     address[] public userPhotoContracts;
 
     function createUserPhoto(uint updateInterval) public returns (address) {
@@ -14,5 +16,16 @@ contract ProfileImageFactory {
 
     function getUserPhotoContracts() public view returns (address[] memory) {
         return userPhotoContracts;
+    }
+
+    function onERC721Received(
+        address operator,
+        address from,
+        uint256 tokenId,
+        bytes memory data
+    ) public virtual override returns (bytes4) {
+        // Implemente a lógica para receber o token ERC721 aqui
+        // e retorne o valor correto da função
+        return this.onERC721Received.selector;
     }
 }
